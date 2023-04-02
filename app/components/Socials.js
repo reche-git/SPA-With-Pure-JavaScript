4db906b8c9cd756bce8716d7dcb3e892f9405717
Alan Reche <alanaxelreche@hotmail.com>
2023-04-02 16:35:40 -0300

    Links added

--- app/components/Socials.js
+++ app/components/Socials.js
@@ -1,25 +1,25 @@
 export function Socials() {
-    const $template = document.createElement("div");
-    $template.innerHTML = `
-    <div class="wrapper btn-floating">
-  <a href="#" class="icon facebook">
+  const $template = document.createElement("div");
+  $template.innerHTML = `
+    <div class="wrapper btn-floating" rel="noreferrer" target="_blank">
+  <a href="https://www.facebook.com/" class="icon facebook" rel="noreferrer" target="_blank">
     <div class="tooltip">Facebook</div>
     <i class="fa fa-facebook" aria-hidden="true"></i>
   </a>
-  <a href="#" class="icon twitter">
+  <a href="https://twitter.com/home?lang=en" class="icon twitter" rel="noreferrer" target="_blank">
     <div class="tooltip">Twitter</div>
     <i class="fa fa-twitter" aria-hidden="true"></i>
   </a>
-  <a href="#" class="icon instagram">
+  <a href="https://github.com/reche-git" class="icon instagram" rel="noreferrer" target="_blank">
     <div class="tooltip">Instagram</div>
     <i class="fa fa-instagram" aria-hidden="true"></i>
   </a>
-  <a href="#" class="icon github">
+  <a href="https://github.com/reche-git" class="icon github" rel="noreferrer" target="_blank">
     <div class="tooltip">Github</div>
     <i class="fa fa-github" aria-hidden="true"></i>
   </a>
 </div>
     `;
 
-    return $template
+  return $template;
 }
